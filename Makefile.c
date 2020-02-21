3822902b36b8103c0182f71174a0c7b72a86d228
Дятлов Кирилл Юрьевич <ip911s10@awp02.csc.local>
2020-02-21 10:34:11 +0700

    Add function P, S

---
+++ Makefile.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+main(){
+	float pi=3.14, S, P;
+	int R;
+	scanf("%d", &R);
+	P=pi*2*R;
+	S=pi*pow(R, 2);
+	printf("Периметр = %f", P);
+        printf("Площадь = %f", S);
+	getchar();
+	return 0;       
+}
+
